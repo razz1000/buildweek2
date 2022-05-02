a8fc490962e8cc34e127dce34402c010689e81f3
stefantanasa <liketanasa@gmail.com>
2022-05-02 12:11:31 +0100

    update

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import ProfileJumbotron from "./Profile-Jumbotron";
+import ProfileJumbotron from "./ProfileJumbotron";
 
 const ProfilePage = () => {
   return (

bc518163e8ff5ee79cd22facaf642283f09c80c0
lbourke1234 <lbourke1234@gmail.com>
2022-05-02 13:53:07 +0100

    push

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -1,9 +1,33 @@
 import SideBar from './SideBar'
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import ProfileJumbotron from "./ProfileJumbotron";
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+import ProfileJumbotron from './Profile-Jumbotron'
+import { useEffect, useState } from 'react'
 
 const ProfilePage = () => {
+  const [allProfiles, setAllProfiles] = useState([])
+
+  useEffect(() => {
+    fetchAllProfiles()
+  }, [])
+
+  const fetchAllProfiles = async () => {
+    const response = await fetch(
+      'https://striveschool-api.herokuapp.com/api/profile/',
+      {
+        headers: {
+          authorization:
+            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjU2YmQxZGE5MDIzOTAwMTVkOTY1YzgiLCJpYXQiOjE2NTE0OTM1NzcsImV4cCI6MTY1MjcwMzE3N30.9qnwR5Y-5lsD8GLJZNjp5T6Z__FJku-we3Sn6MwRpp0'
+        }
+      }
+    )
+
+    const data = await response.json()
+
+    console.log(data.slice(0, 8))
+    setAllProfiles(data)
+  }
+
   return (
     <Container>
       <Row>
@@ -11,7 +35,7 @@ const ProfilePage = () => {
           <ProfileJumbotron />
         </Col>
         <Col md={4}>
-          <SideBar heading="People also viewed" />
+          <SideBar data={allProfiles} heading="People also viewed" />
         </Col>
       </Row>
     </Container>
--- src/components/SinglePerson.jsx
+++ src/components/SinglePerson.jsx
@@ -1,27 +1,26 @@
 import { Container, Row, Col, Button } from 'react-bootstrap'
 import './stylesheets/singlePerson.css'
 
-const SinglePerson = () => {
+const SinglePerson = (props) => {
   return (
     <Container fluid className="py-1">
       <Row>
-        <Col md={2}>
+        <Col md={3}>
           <img
             style={{ height: '50px', width: '50px', borderRadius: '50%' }}
-            src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fsm.askmen.com%2Ft%2Faskmen_in%2Farticle%2Ff%2Ffacebook-p%2Ffacebook-profile-picture-affects-chances-of-gettin_fr3n.1200.jpg&f=1&nofb=1"
+            src={props.profile.image}
             alt="profilePic"
           />
         </Col>
-        <Col md={10}>
+        <Col md={9}>
           <Row>
-            <span className="name">John O'Brien</span>{' '}
+            <span className="name">
+              {props.profile.name} {props.profile.surname}
+            </span>{' '}
             <span className="dash">-</span> <span className="number">3rd</span>
           </Row>
           <Row>
-            <p className="occupation">
-              Lead Biotech Production Specialist at Regeneron Pharmaceuticals,
-              Inc.
-            </p>
+            <p className="occupation">{props.profile.title}</p>
           </Row>
           <Row>
             <Button variant="outline-secondary" className="rounded-pill">

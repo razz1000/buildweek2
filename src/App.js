24e5d431fab841039436f7abc460a3e51b26eb61
razz1000 <76906581+razz1000@users.noreply.github.com>
2022-05-02 09:18:07 +0100

    Initial setup

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
+import ProfilePage from "./components/ProfilePage"
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      
       <Routes>
-        <Route />
+        <Route path={"/profilepage"} element={<ProfilePage />} />
 
       </Routes>
       
---
+++ src/components/ProfilePage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import {Container, Row, Col} from "react-bootstrap"
+
+
+const ProfilePage = () => {
+    return(
+        <Container>
+            <Row>
+                <Col md={8}></Col>
+                <Col md={4}></Col>
+            </Row>
+
+        </Container>
+
+    )
+}
+
+export default ProfilePage

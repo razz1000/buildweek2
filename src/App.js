8569e78d850779540afd16213aa9719b6cd6832c
lbourke1234 <lbourke1234@gmail.com>
2022-05-02 11:57:48 +0100

    push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter } from 'react-router-dom';
-import ProfilePage from "./components/ProfilePage"
+import logo from './logo.svg'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import ProfilePage from './components/ProfilePage'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      
-      <Routes>
-        <Route path={"/profilepage"} element={<ProfilePage />} />
-
-      </Routes>
-      
-      
+        <Routes>
+          <Route path={'/profile-page'} element={<ProfilePage />} />
+        </Routes>
       </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -1,18 +1,17 @@
-import React from "react";
-import {Container, Row, Col} from "react-bootstrap"
-
-
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+import SideBar from './SideBar'
 const ProfilePage = () => {
-    return(
-        <Container>
-            <Row>
-                <Col md={8}></Col>
-                <Col md={4}></Col>
-            </Row>
-
-        </Container>
-
-    )
+  return (
+    <Container>
+      <Row>
+        <Col md={8}></Col>
+        <Col md={4}>
+          <SideBar heading="People also viewed" />
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 
 export default ProfilePage

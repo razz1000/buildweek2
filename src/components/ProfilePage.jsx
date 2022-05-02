3a7340812e4e435c4faa6a35e55c1b3ef4871826
lbourke1234 <lbourke1234@gmail.com>
2022-05-02 15:26:22 +0100

    push after navbar again

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -1,33 +1,33 @@
 import SideBar from './SideBar'
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
-import ProfileJumbotron from './Profile-Jumbotron'
+import ProfileJumbotron from './ProfileJumbotron'
 import { useEffect, useState } from 'react'
 import SidebarTop from './SidebarTop'
 import SidebarExtraSection from './SidebarExtraSection'
 const ProfilePage = () => {
-  const [allProfiles, setAllProfiles] = useState([]);
+  const [allProfiles, setAllProfiles] = useState([])
 
   useEffect(() => {
-    fetchAllProfiles();
-  }, []);
+    fetchAllProfiles()
+  }, [])
 
   const fetchAllProfiles = async () => {
     const response = await fetch(
-      "https://striveschool-api.herokuapp.com/api/profile/",
+      'https://striveschool-api.herokuapp.com/api/profile/',
       {
         headers: {
           authorization:
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjU2YmQxZGE5MDIzOTAwMTVkOTY1YzgiLCJpYXQiOjE2NTE0OTM1NzcsImV4cCI6MTY1MjcwMzE3N30.9qnwR5Y-5lsD8GLJZNjp5T6Z__FJku-we3Sn6MwRpp0",
-        },
+            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjU2YmQxZGE5MDIzOTAwMTVkOTY1YzgiLCJpYXQiOjE2NTE0OTM1NzcsImV4cCI6MTY1MjcwMzE3N30.9qnwR5Y-5lsD8GLJZNjp5T6Z__FJku-we3Sn6MwRpp0'
+        }
       }
-    );
+    )
 
-    const data = await response.json();
+    const data = await response.json()
 
-    console.log(data.slice(0, 8));
-    setAllProfiles(data);
-  };
+    console.log(data.slice(0, 8))
+    setAllProfiles(data)
+  }
 
   return (
     <Container>
@@ -49,7 +49,7 @@ const ProfilePage = () => {
         </Col>
       </Row>
     </Container>
-  );
-};
+  )
+}
 
-export default ProfilePage;
+export default ProfilePage

cf81e5c8d2757c36545c2995731822e5af170783
stefantanasa <liketanasa@gmail.com>
2022-05-05 16:30:44 +0100

    update

--- src/components/AddExperience.jsx
+++ src/components/AddExperience.jsx
@@ -1,54 +1,55 @@
 import React from "react";
 import { Form } from "react-bootstrap";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
-const AddExperience = ({ user, edituserdata }) => {
-  const [userExperience, setUserExperience] = useState(user);
+const AddExperience = ({ experience, setPostExp }) => {
+  const [userExperience, setUserExperience] = useState(experience);
 
-  useEffect(() => setUserExperience(user), []);
-  //   console.log(userExperience);
   let change = (e, field) => {
     setUserExperience({
       ...userExperience,
       [field]: e.target.value,
     });
+    setPostExp({
+      ...userExperience,
+      [field]: e.target.value,
+    });
+
+    console.log("inside setUserExp: ", userExperience);
   };
-  console.log("User Experience: ", userExperience);
-  useEffect(() => edituserdata(userExperience), [userExperience]);
+
   return (
     <Form>
+      <h1>Add Experience</h1>
       <Form.Group>
         <br />
         <Form.Control
-          value={userExperience.role}
           type="text"
           placeholder="Role"
           onChange={(e) => change(e, "role")}
         />
         <br />
         <Form.Control
-          value={userExperience.company}
           type="text"
           placeholder="Company"
           onChange={(e) => change(e, "company")}
         />
         <br />
         <Form.Control
-          value={userExperience.startDate}
+          value="2019-06-16"
           type="date"
           placeholder="Start date"
           onChange={(e) => change(e, "startDate")}
         />
         <br />
         <Form.Control
-          value={userExperience.endDate}
           type="date"
           placeholder="End date"
           onChange={(e) => change(e, "endDate")}
         />
         <br />
         <Form.Group
-          value={userExperience.description}
+          type="text"
           controlId="exampleForm.ControlTextarea1"
           onChange={(e) => change(e, "description")}
         >
@@ -56,7 +57,6 @@ const AddExperience = ({ user, edituserdata }) => {
         </Form.Group>
         <br />
         <Form.Control
-          value={userExperience.area}
           type="text"
           placeholder="Location"
           onChange={(e) => change(e, "area")}

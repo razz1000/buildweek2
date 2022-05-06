0cd90d103614e2e56cce5af9be184d87985bc7da
lbourke1234 <lbourke1234@gmail.com>
2022-05-06 09:51:32 +0100

    push

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -1,94 +1,94 @@
-import SideBar from "./SideBar";
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import ProfileJumbotron from "./ProfileJumbotron";
-import { useEffect, useState } from "react";
-import SidebarTop from "./SidebarTop";
-import SidebarExtraSection from "./SidebarExtraSection";
-import { useParams } from "react-router-dom";
-import ExperiencesCard from "./ExperiencesCard";
+import SideBar from './SideBar'
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+import ProfileJumbotron from './ProfileJumbotron'
+import { useEffect, useState } from 'react'
+import SidebarTop from './SidebarTop'
+import SidebarExtraSection from './SidebarExtraSection'
+import { useParams } from 'react-router-dom'
+import ExperiencesCard from './ExperiencesCard'
 
 const ProfilePage = ({ profiledata, setprofiledata }) => {
-  const [allProfiles, setAllProfiles] = useState([]);
-  const [experiencesId, setExperiencesId] = useState("");
-  const [profile, setProfile] = useState();
+  const [allProfiles, setAllProfiles] = useState([])
+  const [experiencesId, setExperiencesId] = useState('')
+  const [profile, setProfile] = useState()
 
   const editProfileData = (e, field) => {
-    console.log(field);
+    console.log(field)
     setprofiledata({
       ...profiledata,
-      [field]: e.target.value,
-    });
-  };
+      [field]: e.target.value
+    })
+  }
 
   const putProfileData = async () => {
     try {
       const response = await fetch(
-        "https://striveschool-api.herokuapp.com/api/profile/",
+        'https://striveschool-api.herokuapp.com/api/profile/',
         {
-          method: "PUT",
+          method: 'PUT',
           body: JSON.stringify(profiledata),
           headers: {
             authorization:
-              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjZmYzMwMzE3YzRlMDAwMTVkN2EwODIiLCJpYXQiOjE2NTE0OTE1ODgsImV4cCI6MTY1MjcwMTE4OH0.yS8YrZCAJfbhN7ye7OAqtaTyteCbwQsztG411czMp8s",
-            "Content-type": "application/json",
-          },
+              'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjZmYzMwMzE3YzRlMDAwMTVkN2EwODIiLCJpYXQiOjE2NTE0OTE1ODgsImV4cCI6MTY1MjcwMTE4OH0.yS8YrZCAJfbhN7ye7OAqtaTyteCbwQsztG411czMp8s',
+            'Content-type': 'application/json'
+          }
         }
-      );
+      )
 
-      const data = await response.json();
+      const data = await response.json()
 
-      setprofiledata(data);
-      console.log("✅Everything went well, infos were updated!", data);
+      setprofiledata(data)
+      console.log('✅Everything went well, infos were updated!', data)
     } catch (error) {
-      console.log("❌ something went wrong: ", error);
+      console.log('❌ something went wrong: ', error)
     }
-  };
+  }
 
   const fetchAllProfiles = async () => {
     const response = await fetch(
-      "https://striveschool-api.herokuapp.com/api/profile/",
+      'https://striveschool-api.herokuapp.com/api/profile/',
       {
         headers: {
           authorization:
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjU2YmQxZGE5MDIzOTAwMTVkOTY1YzgiLCJpYXQiOjE2NTE0OTM1NzcsImV4cCI6MTY1MjcwMzE3N30.9qnwR5Y-5lsD8GLJZNjp5T6Z__FJku-we3Sn6MwRpp0",
-        },
+            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjU2YmQxZGE5MDIzOTAwMTVkOTY1YzgiLCJpYXQiOjE2NTE0OTM1NzcsImV4cCI6MTY1MjcwMzE3N30.9qnwR5Y-5lsD8GLJZNjp5T6Z__FJku-we3Sn6MwRpp0'
+        }
       }
-    );
+    )
 
-    const data = await response.json();
+    const data = await response.json()
 
-    console.log(data.slice(0, 8));
-    setAllProfiles(data);
-  };
+    console.log(data.slice(0, 8))
+    setAllProfiles(data)
+  }
   const fetchProfileData = async (userId) => {
-    const linkToFetch = `https://striveschool-api.herokuapp.com/api/profile/${userId}`;
+    const linkToFetch = `https://striveschool-api.herokuapp.com/api/profile/${userId}`
     const response = await fetch(linkToFetch, {
       headers: {
         authorization:
-          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjZmYzMwMzE3YzRlMDAwMTVkN2EwODIiLCJpYXQiOjE2NTE0OTE1ODgsImV4cCI6MTY1MjcwMTE4OH0.yS8YrZCAJfbhN7ye7OAqtaTyteCbwQsztG411czMp8s",
-      },
-    });
+          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjZmYzMwMzE3YzRlMDAwMTVkN2EwODIiLCJpYXQiOjE2NTE0OTE1ODgsImV4cCI6MTY1MjcwMTE4OH0.yS8YrZCAJfbhN7ye7OAqtaTyteCbwQsztG411czMp8s'
+      }
+    })
 
-    const data = await response.json();
+    const data = await response.json()
 
-    setProfile(data);
-  };
+    setProfile(data)
+  }
   useEffect(() => {
-    fetchAllProfiles();
+    fetchAllProfiles()
     if (params) {
-      fetchProfileData(params.userId);
-      console.log("profile is fetched again! ");
+      fetchProfileData(params.userId)
+      console.log('profile is fetched again! ')
     } else {
-      setProfile(profiledata);
-      console.log("profile is set from profiledata");
+      setProfile(profiledata)
+      console.log('profile is set from profiledata')
     }
-  }, []);
-  let params = useParams();
+  }, [])
+  let params = useParams()
 
   return (
     <Container>
-      {console.log("this data is important:", profile)}
+      {console.log('this data is important:', profile)}
       <Row>
         <Col md={8}>
           {profile ? (
@@ -133,7 +133,7 @@ const ProfilePage = ({ profiledata, setprofiledata }) => {
         </Col>
       </Row>
     </Container>
-  );
-};
+  )
+}
 
-export default ProfilePage;
+export default ProfilePage
